0b4489d055a994a3776371226dd80147abb55600
jcheron <myaddressmail@gmail.com>
2021-12-22 12:13:52 +0100

    Update ContentSecurityManager.php

--- src/Ubiquity/security/csp/ContentSecurityManager.php
+++ src/Ubiquity/security/csp/ContentSecurityManager.php
@@ -1,9 +1,20 @@
 <?php
 namespace Ubiquity\security\csp;
 
+/**
+ * Manage Content Security Policies.
+ * Ubiquity\security\csp$ContentSecurityManager
+ * This class is part of Ubiquity
+ *
+ * @author jc
+ * @version 1.0.0
+ *
+ */
 class ContentSecurityManager {
 
 	private static NonceGenerator $nonceGenerator;
+
+	private static array $csp = [];
 
 	public static function start(string $nonceGeneratorClass = null) {
 		$nonceGeneratorClass ??= NonceGenerator::class;
@@ -16,6 +27,20 @@ class ContentSecurityManager {
 
 	public static function isStarted(): bool {
 		return isset(self::$nonceGenerator);
+	}
+
+	public static function addCsp(?bool $reportOnly = null): ContentSecurity {
+		return self::$csp[] = new ContentSecurity($reportOnly);
+	}
+
+	public static function clearCsp() {
+		self::$csp = [];
+	}
+
+	public static function addHeadersToResponse(?bool $reportOnly = null): void {
+		foreach (self::$csp as $csp) {
+			$csp->addHeaderToResponse($reportOnly);
+		}
 	}
 }
 
